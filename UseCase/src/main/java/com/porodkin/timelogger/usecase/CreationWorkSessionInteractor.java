014bacb0d96a2cba3481ac54226cd0518d61e173
Oleg Porodkin <stixlive59@gmail.com>
2024-10-15 20:24:16 -0230

    Add logging
    add simple slf4j logging for project

--- UseCase/src/main/java/com/porodkin/timelogger/usecase/CreationWorkSessionInteractor.java
+++ UseCase/src/main/java/com/porodkin/timelogger/usecase/CreationWorkSessionInteractor.java
@@ -6,19 +6,22 @@ import com.porodkin.timelogger.domain.fabric.WorkSessionFabric;
 import com.porodkin.timelogger.usecase.datastuct.input.CreateWorkSessionInputData;
 import com.porodkin.timelogger.usecase.datastuct.output.CreatedWorkSessionOutputData;
 import com.porodkin.timelogger.usecase.fabric.CreatedWorkSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class CreationWorkSessionInteractor implements CreationWorkSessionInputBoundary {
+    private static final Logger log = LoggerFactory.getLogger(CreationWorkSessionInteractor.class);
 
     public static final String SUCCESSFUL_MESSAGE = "Work session successfully created.";
     public static final String FAILED_MESSAGE = "Failed to create work session, Please fill the start date and time.";
 
     private final DataAccessBoundary dataAccess;
-    private final CreationWorkSessionOutputBoundary addWorkedTimePresenter;
+    private final CreationWorkSessionOutputBoundary<?> addWorkedTimePresenter;
 
-    public CreationWorkSessionInteractor(DataAccessBoundary dataAccess, CreationWorkSessionOutputBoundary addWorkedTimePresenter) {
+    public CreationWorkSessionInteractor(DataAccessBoundary dataAccess, CreationWorkSessionOutputBoundary<?> addWorkedTimePresenter) {
         this.dataAccess = dataAccess;
         this.addWorkedTimePresenter = addWorkedTimePresenter;
     }
@@ -32,11 +35,13 @@ public class CreationWorkSessionInteractor implements CreationWorkSessionInputBo
         try {
             workSession = WorkSessionFabric.create(uuid, workedTimeSlice);
         } catch (CreationWorkSessionException e) {
-            CreatedWorkSessionOutputData outputData = CreatedWorkSessionFactory.create(false, null, FAILED_MESSAGE);
+            log.warn("Can't creation work session, date/time don't set", e);
+            CreatedWorkSessionOutputData outputData = CreatedWorkSessionFactory.create(Boolean.TRUE, null, FAILED_MESSAGE);
             addWorkedTimePresenter.presentCreatedWorkSession(outputData);
         }
         dataAccess.saveWorkTime(workSession);
 
+        log.info("Work session created: {}", uuid);
         CreatedWorkSessionOutputData outputData = CreatedWorkSessionFactory.create(Boolean.TRUE, uuid.toString(), SUCCESSFUL_MESSAGE);
         addWorkedTimePresenter.presentCreatedWorkSession(outputData);
     }
